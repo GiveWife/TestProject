42e1348ccacbc0d5a0facfeb5b42636ecd8cd529
Nicolas <30669856+GiveWife@users.noreply.github.com>
2024-03-10 15:41:45 +0100

    s

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Formatter;
@@ -7,6 +9,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         File file = new File("res/test.txt");
+
+        /*
         System.out.println("Before:");
         System.out.println("format: 68656c6c6f686466");
         byte[] bytes = Files.readAllBytes(file.toPath());
@@ -25,7 +29,41 @@ public class Main {
 
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
-        System.out.println("Delimeter: " + identifyLineDelimiter(formattedContent));
+        System.out.println("Delimeter: " + identifyLineDelimiter(formattedContent));*/
+
+        try {
+            byte[] fileContent = Files.readAllBytes(file.toPath());
+
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String s = "";
+            String sub = "";
+            while((sub = reader.readLine()) != null) {
+                s += sub;
+            }
+
+            Formatter formatter = new Formatter();
+            for(byte b : fileContent) formatter.format("%02x", b);
+            System.out.println("Read out bytes:: " + formatter.toString());
+
+            Formatter formatter2 = new Formatter();
+            byte[] stringToBytes = s.getBytes();
+            for(byte b : stringToBytes) formatter2.format("%02x", b);
+            System.out.println("String to bytes: " + formatter2.toString());
+
+
+        } catch(IOException e) {
+
+        }
+
+        String alpha = " !\"#$%&\\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+        for(int i = 0; i < alpha.length(); i++) {
+            Formatter f = new Formatter();
+            byte[] bytesForChar = alpha.substring(i, i+1).getBytes();
+            for(byte b : bytesForChar) f.format("%02x", b);
+            String byteForm = f.toString();
+            System.out.println("Letter: '" + alpha.substring(i, i + 1) + "': " + byteForm);
+        }
+
 
 
     }
